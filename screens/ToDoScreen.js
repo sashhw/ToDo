f3d07a398d50c18851692acc947b3608a009fdae
swalkowski <swalkowski@foxtrotco.com>
2024-01-29 10:51:41 -0600

    added "ToDoScreen" file, implemented this screen in App.js

---
+++ screens/ToDoScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ToDoScreen = () => {
+  return (
+    <View>
+      <Text>this is text that will be the to do list</Text>
+    </View>
+  );
+};
+
+export default ToDoScreen;
